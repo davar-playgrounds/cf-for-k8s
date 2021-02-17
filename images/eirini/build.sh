83342e9b937a45073b08da737ca4edaa0ef6a45f
Andrew Wittrock <awittrock@vmware.com>
2021-02-16 16:34:33 -0800

    More WIP
    Signed-off-by: Andrew Costa <ancosta@vmware.com>

--- images/eirini/build.sh
+++ images/eirini/build.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 
-ytt -f kbld-template.yml -f kbld-data-values.yml > kbld-config.yml
+pushd repo > /dev/null
+  GIT_SHA=$(git rev-parse HEAD)
+popd > /dev/null
+
+echo "yttifying"
+ytt -f kbld-template.yml -f kbld-data-values.yml -v "eirini.git_sha=${GIT_SHA}" > kbld-config.yml
+echo "kblding"
+kbld -f kbld-config.yml -f images.yml --lock-output kbld.lock.yml > kbld-output.yml
+echo "is great success!"
